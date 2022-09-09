1a5cf71836c04e7e7e9473430ad1263f68b00993
Anh-Toine <52642269+Anh-Toine@users.noreply.github.com>
2022-09-09 12:39:18 -0400

    Initial Commit

---
+++ BMCalc/BMCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMCalcApp.swift
+//  BMCalc
+//
+//  Created by Lukas Nguyen on 2022-09-09.
+//
+
+import SwiftUI
+
+@main
+struct BMCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BMCalc/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BMCalc
+//
+//  Created by Lukas Nguyen on 2022-09-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
